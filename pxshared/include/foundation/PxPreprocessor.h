a303ccc474602cccf17bb5855ba968b15abe2464
James Chen <dumganhar@gmail.com>
2025-02-22 17:16:03 +0800

    Update ios simulator

--- pxshared/include/foundation/PxPreprocessor.h
+++ pxshared/include/foundation/PxPreprocessor.h
@@ -109,6 +109,11 @@ Operating system defines, see http://sourceforge.net/p/predef/wiki/OperatingSyst
 		#define PX_OSX 1
 	#elif TARGET_OS_IPHONE || TARGET_OS_SIMULATOR
 		#define PX_IOS 1
+		#ifdef TARGET_OS_SIMULATOR
+			#define PX_IOS_SIMULATOR 1
+		#else
+			#define PX_IOS_SIMULATOR 0
+		#endif
 	#else
 		#define PX_OSX 1
 	#endif
