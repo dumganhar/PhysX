fc2b17170f617264276cd3b2c54ccb998e15df7a
James Chen <dumganhar@gmail.com>
2025-02-22 16:36:38 +0800

    Fix warning for PX_OPENHARMONY

--- physx/source/foundation/src/unix/PsUnixThread.cpp
+++ physx/source/foundation/src/unix/PsUnixThread.cpp
@@ -274,7 +274,7 @@ void ThreadImpl::kill()
 #ifndef ANDROID
 	if(getThread(this)->state == _PxThreadStarted)
 	{
-		#if PX_OPENHARMONY
+		#if defined(PX_OPENHARMONY) && PX_OPENHARMONY
 		pthread_kill(getThread(this)->thread, 0);
 		#else
 		pthread_cancel(getThread(this)->thread);
