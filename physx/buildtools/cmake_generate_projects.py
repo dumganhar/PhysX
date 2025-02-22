143e580608ea179aff5abe2f741a3f3d1d36837c
James Chen <dumganhar@gmail.com>
2025-02-22 13:36:59 +0800

    Update mac build

--- physx/buildtools/cmake_generate_projects.py
+++ physx/buildtools/cmake_generate_projects.py
@@ -337,7 +337,7 @@ class CMakePreset:
             return outString
         elif self.targetPlatform == 'mac64':
             outString = outString + ' -DTARGET_BUILD_PLATFORM=mac'
-            outString = outString + ' -DPX_OUTPUT_ARCH=x86'
+            # outString = outString + ' -DPX_OUTPUT_ARCH=x86'
             return outString
         elif self.targetPlatform == 'ios64':
             outString = outString + ' -DTARGET_BUILD_PLATFORM=ios'
